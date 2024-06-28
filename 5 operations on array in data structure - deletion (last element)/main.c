64fbbc146f4f41c426e7aa16e1900d9b84d24701
Abhisek Kumar <123446305+AbhisekWebDev@users.noreply.github.com>
2024-06-28 10:39:07 +0530

    Add files via upload

---
+++ 5 operations on array in data structure - deletion (last element)/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[50], size, num, pos;
+
+    printf("Enter the size of array : ");
+    scanf("%d", &size);
+
+    if(size > 50)
+        printf("Invalid size");
+    else {
+    printf("Enter the elements of array : ");
+
+    for(int i = 0 ; i < size ; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter position you want to delete : ");
+    scanf("%d", &pos);
+
+    if(pos < 0 || pos > size)
+        printf("Invalid position");
+    else {
+        size--;
+    }
+    }
+
+    for(int i = 0 ; i < size ; i++)
+        printf("%d", a[i]);
+
+    return 0;
+}
