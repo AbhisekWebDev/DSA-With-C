da296fdd3c54dcaba9c0b27677714703ebaa1137
Abhisek Kumar <123446305+AbhisekWebDev@users.noreply.github.com>
2024-07-04 11:24:22 +0530

    Add files via upload

---
+++ 13 linked list - deletion at begining/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    struct node{
+
+        int data;
+        struct node *next;
+    };
+
+    struct node *head = 0, *newNode, *temp;
+
+    int choice = 1, count = 0;
+
+    while(choice){
+
+        newNode = (struct node*)malloc(sizeof(struct node));
+
+        printf("Enter data \n");
+        scanf("%d", &newNode -> data);
+
+        newNode -> next = 0;
+
+        if(head == 0){
+
+            head = temp = newNode;
+        } else {
+
+            temp -> next = newNode;
+            temp = newNode;
+        }
+
+        printf("0 to abort & 1 to continue (0, 1) \n");
+        scanf("%d", &choice);
+    }
+
+    temp = head;
+
+    printf("Entered data: \n");
+
+    while(temp != 0){
+
+        printf("%d ", temp -> data);
+
+        temp = temp -> next;
+    }
+
+    printf("\n");
+
+    // delete a node at the beginning
+
+    temp = head;
+
+    head = head -> next;
+
+    free(temp);
+
+
+    printf("Remaining data after deletion from beginning : \n");
+
+
+    temp = head;
+
+    while(temp != 0){
+
+        printf("%d ", temp->data);
+
+        temp = temp->next;
+
+        count++;
+    }
+
+    printf("\nTotal number of nodes: %d\n", count);
+
+    return 0;
+}
+
+
+
+
