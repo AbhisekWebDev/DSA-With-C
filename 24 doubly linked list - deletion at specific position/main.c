64d90d0bcddd17d4b8331261b61653059e9d424e
Abhisek Kumar <123446305+AbhisekWebDev@users.noreply.github.com>
2024-07-08 11:53:25 +0530

    Add files via upload

---
+++ 24 doubly linked list - deletion at specific position/main.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node {
+
+    int data;
+
+    struct node *prev, *next;
+};
+
+struct node *head, *newnode, *temp, *tail;
+
+int choice = 1, count;
+
+
+int main()
+{
+    create();
+    display();
+
+    deleteAtSpecificPosition();
+    display();
+
+    return 0;
+}
+
+
+void create(){
+
+    head = 0;
+
+    while(choice){
+
+        newnode = (struct node *)malloc(sizeof(struct node));
+
+        printf("Enter data : ");
+        scanf("%d", &newnode -> data);
+
+        newnode -> prev = newnode -> next = 0;
+
+        if(head == 0){
+
+            head = tail = newnode;
+        }
+        else{
+
+            tail -> next = newnode;
+            newnode -> prev = tail;
+            tail = newnode;
+        }
+
+        printf("0 to abort & 1 to continue (0, 1) \n");
+        scanf("%d", &choice);
+    }
+}
+
+
+void deleteAtSpecificPosition(){
+
+    int pos, i = 1;
+
+    printf("Enter position : ");
+    scanf("%d", &pos);
+
+
+    temp = head;
+
+
+    printf("Remaining data after deletion from the specific position : \n");
+
+
+    while(i < pos){
+
+        temp = temp -> next;
+        i++;
+    }
+
+    temp -> prev -> next = temp -> next;
+    temp -> next -> prev = temp -> prev;
+
+
+    free(temp);
+}
+
+
+void display(){
+
+    count = 0;
+
+    temp = head;
+
+    printf("Entered data: ");
+
+    while(temp != 0){
+
+        printf("%d", temp -> data);
+
+        temp = temp -> next;
+
+        count++;
+    }
+
+    printf("\nTotal number of nodes: %d\n", count);
+}
+
