f7b9422fd7949d16714d9a6037a575dbf6dd8a0f
Abhisek Kumar <123446305+AbhisekWebDev@users.noreply.github.com>
2024-07-12 11:30:52 +0530

    Add files via upload

---
+++ 37 doubly circular linked list - insertion at end/main.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *prev, *next;
+} *head, *tail, *temp, *newnode;
+
+int main()
+{
+    create();
+    display();
+
+    insertAtEnd();
+    display();
+
+    return 0;
+}
+
+void create() {
+    int choice = 1;
+
+    while(choice){
+
+        newnode = (struct node *)malloc(sizeof(struct node));
+
+        printf("Enter data : ");
+        scanf("%d", &newnode -> data);
+
+        if(head == 0){
+            head = tail = newnode;
+            head -> next = head;
+            head -> prev = head;
+        }else{
+            tail -> next = newnode;
+            newnode -> prev = tail;
+            newnode -> next = head; // head hi rkhna kyunki head always points to the first node in the list
+            head -> prev = newnode;
+            tail = newnode;
+        }
+
+        printf("0 to abort & 1 to continue (0, 1): ");
+        scanf("%d", &choice);
+    }
+
+    printf("Accessing the first data in the list through the tail pointer : %d \n", tail -> next -> data);
+    printf("Accessing the last data in the list through the head pointer :%d \n", head -> prev -> data);
+}
+
+void insertAtEnd() {
+    newnode = (struct node *)malloc(sizeof(struct node));
+
+    printf("Enter data at the end : ");
+    scanf("%d", &newnode -> data);
+
+    if(head == 0){
+        head = tail = newnode;
+        newnode -> prev = tail;
+        newnode -> next = head;
+    }else{
+        newnode -> next = head;
+        tail -> next = newnode;
+        newnode -> prev = tail;
+        head -> prev = newnode;
+        tail = newnode;
+    }
+
+    printf("Data after insertion at the end : ");
+}
+
+void display() {
+    temp = head;
+
+    int count = 0;
+
+    if(head == 0) {
+        printf("List is empty\n");
+    }
+
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+        count++;
+    } while(temp != head);
+
+    printf("\nTotal number of nodes : %d\n", count);
+}
